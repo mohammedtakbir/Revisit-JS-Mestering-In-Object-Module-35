5903fa8922cf47385b6f34a8022c1e5e8dc34476
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-28 19:19:50 +0600

    explore Object.keys/values/entries/seal/freeze/delete

---
+++ practice/freeze.js
@@ -0,0 +1,27 @@
+const phone = {name: 'Galaxy S23+', storage: '256GB', ram: '8GB', display: '6.6inch', battery: '4700mah'};
+
+//* Object.keys()
+// console.log(Object.keys(phone))
+
+//* Object.values()
+// console.log(Object.values(phone))
+
+//* Object.entries()
+// console.log(Object.entries(phone))
+
+//* Object.seal()
+// Object.seal(phone);
+// phone.camera = '50MP';
+// phone.ram = '12GB'
+// delete phone.ram;
+// console.log(phone)
+
+//* Object.freeze()
+// Object.freeze(phone);
+// phone.battery = '5000mah'
+// console.log(phone)
+
+//* object delete
+// console.log(phone)
+// delete phone.ram;
+// console.log(phone)
